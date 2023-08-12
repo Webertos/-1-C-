18f1599f6f8c4566960eaa5a9df43e57b0b0bbaa
webertos <max.wow277@gmail.com>
2023-08-12 22:00:13 +0300

    home working

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿/* int num1 = int.Parse(Console.ReadLine());
+ int num2 = int.Parse(Console.ReadLine());
+
+if (num1 < num2)
+{
+    Console.WriteLine(num2);
+}
+if (num2 < num1)
+{
+    Console.WriteLine(num1);
+}
+*/
+
+/*int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 < num2 && num2 > num3)
+{
+   Console.WriteLine(num2);
+}
+if (num2 < num3 && num1 < num2)
+{
+   Console.WriteLine(num3);
+}
+if (num1 > num2 && num2 > num3)
+{
+   Console.WriteLine(num1);
+}
+*/
+
+
+/*int num1 = int.Parse(Console.ReadLine());
+
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("Chetnoe");
+
+}
+else
+{
+    Console.WriteLine("Nechetnoe");
+}
+*/
+
+
+/*int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+
+for (int i = num2; i <= num1; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+        
+    }
+}
+*/
+
